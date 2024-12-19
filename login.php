48dd9ed0d287915e7ee509b0fa63c486f0664fc0
hitomi-takebe <takebe.hitomi@gmail.com>
2024-12-20 02:26:17 +0900

    test

--- login.php
+++ login.php
@@ -21,25 +21,25 @@ try {
 // 指定したハッシュがパスワードにマッチしているかチェック
 //$_SESSIONはログイン中のユーザー情報を一時的に保持するために使う変数
 //生のパスワードがハッシュ化されたパスワードと一致するかどうかをチェック
-$sql = "SELECT * FROM account WHERE email = :email";
-$stmt = $pdo->prepare($sql);
-$stmt->bindValue(':email', $email);
-$stmt->execute();
-$member = $stmt->fetch();
-//指定したハッシュがパスワードにマッチしているかチェック
-if (password_verify($_POST['password'], $member['password'])) {
-    //DBのユーザー情報をセッションに保存
-    $_SESSION['id'] = $member['id'];
-    $_SESSION['name'] = $member['name'];
-    $msg = 'ログインしました。';
-    $link = '<a href="index.php">ホーム</a>';
-} else {
-    $msg = 'メールアドレスもしくはパスワードが間違っています。';
-    $link = '<a href="login_form.php">戻る</a>';
-}
-?>
+// $sql = "SELECT * FROM account WHERE email = :email";
+// $stmt = $pdo->prepare($sql);
+// $stmt->bindValue(':email', $email);
+// $stmt->execute();
+// $member = $stmt->fetch();
+// //指定したハッシュがパスワードにマッチしているかチェック
+// if (password_verify($_POST['password'], $member['password'])) {
+//     //DBのユーザー情報をセッションに保存
+//     $_SESSION['id'] = $member['id'];
+//     $_SESSION['name'] = $member['name'];
+//     $msg = 'ログインしました。';
+//     $link = '<a href="index.php">ホーム</a>';
+// } else {
+//     $msg = 'メールアドレスもしくはパスワードが間違っています。';
+//     $link = '<a href="login_form.php">戻る</a>';
+// }
+// ?>
 
-<p><?php echo h($_SESSION['name']); ?>さん、こんにちは。</p>
-<h1><?php echo $msg; ?></h1>
-<?php echo $link; ?>
+// <p><?php echo h($_SESSION['name']); ?>さん、こんにちは。</p>
+// <h1><?php echo $msg; ?></h1>
+// <?php echo $link; ?>
 
