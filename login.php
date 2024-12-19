bfeaa75771286590e654b91640ede2b4d9e0d548
hitomi-takebe <takebe.hitomi@gmail.com>
2024-12-20 02:35:41 +0900

    test2

--- login.php
+++ login.php
@@ -11,13 +11,13 @@ $db_host = 'mysql3104.db.sakura.ne.jp';
 $db_id = 'yoin_account';
 $db_pw ='deploy_yoin';
 
-//1.  DB接続します
-//try catch構文でデータベースの情報取得を実施
-// try {
-//   $pdo = new PDO('mysql:dbname='.$db_name.';charset=utf8;host='.$db_host,$db_id,$db_pw);
-// } catch (PDOException $e) {
-//   exit('DBConnectError:'.$e->getMessage());
-// }
+// 1.  DB接続します
+// try catch構文でデータベースの情報取得を実施
+try {
+  $pdo = new PDO('mysql:dbname='.$db_name.';charset=utf8;host='.$db_host,$db_id,$db_pw);
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
 
 // 指定したハッシュがパスワードにマッチしているかチェック
 //$_SESSIONはログイン中のユーザー情報を一時的に保持するために使う変数
