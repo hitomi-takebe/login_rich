6eff7576affe924291a95d7ab79cf501ed1bcfaf
hitomi-takebe <takebe.hitomi@gmail.com>
2024-12-20 03:22:05 +0900

    デザイン修正１

--- php/login_form.php
+++ php/login_form.php
@@ -1,16 +1,33 @@
-<h1>ログインページ</h1>
+<!-- <h1>ログインページ</h1>
 <form action="login.php" method="post">
 <div>
     <label>
         メールアドレス：
-        <input type="email" name="email" required>
+        <input type="email" class="email" name="email" required>
     </label>
 </div>
 <div>
     <label>
         パスワード：
-        <input type="password" name="password" required>
+        <input type="password" class="password"  name="password" required>
     </label>
 </div>
 <input type="submit" value="ログイン">
-</form>
+</form> -->
+
+<body>
+    <div class="card">
+        <span class="card__title">ログインページ</span>
+        <p class="card__content">サービスを使うためにログインをお願いします。</p>
+        <div class="card__form">
+            <form action="php/index_confirm.php" method="post">
+            <label for="email">メールアドレス:</label>
+            <input type="email" class="email" name="email" required>
+            <label for="password">パスワード:</label>
+            <input type="password" class="password" name="password" placeholder="Your Password"><br>
+            <input type="submit" class="submit" value="ログイン">
+            </form>
+        <p class="submit"><a href="../index.php">会員登録をする</a></p>
+        </div>
+    </div>
+</body>
